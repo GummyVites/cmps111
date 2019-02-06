f813ee41733ecda2415f5473cdfe418696479868
Kevin Lee <kemalee@ucsc.edu>
2019-02-06 13:32:18 -0800

    hi

--- asgn2/build.sh
+++ asgn2/build.sh
@@ -1,5 +1,5 @@
-cp /usr/home/kemalee/alyehuan/asgn2/kern_switch.c /usr/src/sys/kern/kern_switch.c
-cp /usr/home/kemalee/alyehuan/asgn2/sched_ule.c  /usr/src/sys/kern/sched_ule.c
+cp /usr/home/kemalee/cmps111/asgn2/kern_switch.c /usr/src/sys/kern/kern_switch.c
+cp /usr/home/kemalee/cmps111/asgn2/sched_ule.c  /usr/src/sys/kern/sched_ule.c
 # cp /usr/home/alyehuan/alyehuan/asgn2/runq.h  /usr/src/sys/sys/runq.h
 cd /usr/src
 make buildkernel -DKERNFAST
